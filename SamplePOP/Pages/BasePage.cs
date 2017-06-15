442279170b41fba08be1a148f71077f119300a2c
JuneCho23 <junecho.ucb@gmail.com>
2017-06-15 09:28:21 -0700

    BasePage

---
+++ SamplePOP/Pages/BasePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.UITest;
+
+namespace SamplePOP.Pages
+{
+	public abstract class BasePage
+	{
+		protected readonly IApp App;
+		protected readonly bool OnAndroid;
+		protected readonly bool OniOS;
+
+		protected BasePage(IApp app, Platform platform)
+		{
+			App = app;
+
+			OnAndroid = platform == Platform.Android;
+			OniOS = platform == Platform.iOS;
+		}
+	}
+}
