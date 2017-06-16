dbada56819adeaa33d1d99e136783b1abace0edc
JuneCho23 <junecho.ucb@gmail.com>
2017-06-15 20:05:44 -0700

    welcome page

--- SamplePOP/Pages/BasePage.cs
+++ SamplePOP/Pages/BasePage.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using Xamarin.UITest;
 
-namespace SamplePOP.Pages
+namespace SamplePOP
 {
-	public abstract class BasePage
-	{
-		protected readonly IApp App;
-		protected readonly bool OnAndroid;
-		protected readonly bool OniOS;
+    public abstract class BasePage
+    {
+        protected readonly IApp App;
+        protected readonly bool OnAndroid;
+        protected readonly bool OniOS;
 
-		protected BasePage(IApp app, Platform platform)
-		{
-			App = app;
+        protected BasePage(IApp app, Platform platform)
+        {
+            App = app;
 
-			OnAndroid = platform == Platform.Android;
-			OniOS = platform == Platform.iOS;
-		}
-	}
+            OnAndroid = platform == Platform.Android;
+            OniOS = platform == Platform.iOS;
+        }
+    }
 }
--- SamplePOP/Pages/WelcomePage.cs
+++ SamplePOP/Pages/WelcomePage.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
 namespace SamplePOP.Pages
 {
     public class WelcomePage
