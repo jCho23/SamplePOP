bb88e98b6591a494b3f6e47994bd756b58d80685
JuneCho23 <junecho.ucb@gmail.com>
2017-06-17 12:19:04 -0700

    Email Page

---
+++ SamplePOP/Pages/EmailPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace SamplePOP
+{
+    public class EmailPage : BasePage
+    {
+        public EmailPage(IApp app, Platform platform) : base(app, platform)
+        {
+        }
+    }
+}
