cd289cf6a1d83e44a1d9bcf2656234747e5df53d
JuneCho23 <junecho.ucb@gmail.com>
2017-06-19 19:03:43 -0700

    using

--- SamplePOP/Pages/UpgradePage.cs
+++ SamplePOP/Pages/UpgradePage.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Linq;
+
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+
 namespace SamplePOP
 {
     public class UpgradePage
